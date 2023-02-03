1006df7673d0cabaed74a79e29703a39d707587f
Lauren Hanson <lohanson33@gmail.com>
2023-02-03 11:06:02 -0600

    changes made to models

--- gameraterapi/models/game_photo.py
+++ gameraterapi/models/game_photo.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class GamePhoto(models.Model):
-    game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='games')
-    player = models.ForeignKey('Player', on_delete=models.CASCADE, related_name='players')
+    game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='game_photos')
+    player = models.ForeignKey('Player', on_delete=models.CASCADE, related_name='player_photos')
     photo_url = models.CharField(max_length = 155)
     
--- gameraterapi/models/game_review.py
+++ gameraterapi/models/game_review.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class GameReview(models.Model): 
-    player = models.ForeignKey('Player', on_delete=models.CASCADE, related_name='players')
-    game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='games')
+    player = models.ForeignKey('Player', on_delete=models.CASCADE, related_name='player_reviews')
+    game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='game_reviews')
     rating = models.IntegerField()
     date_reviewed = models.DateField()
